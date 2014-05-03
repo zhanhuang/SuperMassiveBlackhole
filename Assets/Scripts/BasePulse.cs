2774820f13b8b98aa88ac6956c7818beb38e2ae5
James Zhang <jzhangx2@gmail.com>
2014-05-02 22:34:09 -0400

    Pulse Changes

---
+++ Assets/Scripts/BasePulse.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasePulse : MonoBehaviour {
+
+	Material pulseMat;
+
+	// Use this for initialization
+	void Start () {
+		pulseMat = transform.renderer.material;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		AnimationState state = transform.GetComponent<Animation>()["Take 001"];
+		float animationTime = state.time;
+
+		Color baseColor = pulseMat.GetColor("_TintColor");
+		pulseMat.SetColor("_TintColor", new Color(baseColor.r,baseColor.g, baseColor.b, Mathf.Clamp01(.85f - (animationTime % state.length)/state.length)));
+	}
+}
